5949eb2f7ecb7c2a4dd761864b754ddef45ddb91
kimo095 <80036845+kimo095@users.noreply.github.com>
2026-07-20 12:24:13 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig } from "@angular/core";
-import { provideRouter, withComponentInputBinding } from "@angular/router";
+import { provideRouter, withComponentInputBinding ,  withRouterConfig } from "@angular/router";
 import { routes } from "./app.routes";
 
 
 export const appConfig :ApplicationConfig = {
     providers:[
-        provideRouter(routes , withComponentInputBinding())
+        provideRouter(routes , withComponentInputBinding() , withRouterConfig({
+        paramsInheritanceStrategy:'always'})),
+      // 
       //here we use withComponentInputBinding() to tell the angular that we will expect input to my routes
+      //here we use  withRouterConfig() to let accessing parent route data from inside nested routes
     ],
 }
