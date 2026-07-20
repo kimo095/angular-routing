989a8b2a85f09b1c950c209794a3918a1d162256
kimo095 <80036845+kimo095@users.noreply.github.com>
2026-07-20 15:00:29 +0200

    Update tasks.component.ts

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -10,12 +10,28 @@ import { Task } from './task/task.model';
   styleUrl: './tasks.component.css',
   imports: [TaskComponent , RouterLink],
 })
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   userId= input.required<string>();
   // exctracting query parameters via inputs
   order = input<'asc' | 'desc'>();
   private taskService = inject(TaskServices);
   userTasks: Task[] = computed(()=>
     this.taskService.allTasks().filter((task)=>task.userId === this.userId())
+                               .sort((a,b)=>{
+                                 if (this.order() === 'desc'){
+                                   return a.id > b.id ? -1 : 1 ;
+                                 } else {
+                                   return a.id > b.id ? 1 : -1 ;
+                                 }
+                               })
       );
+  // exctracting query parameters via observables
+  // private activatedRoute = inject(ActivatedRoute);
+  // private destroyRef = inject(DestroyRef);
+  // ngOnInit():void{
+  // const subscription =   this.activatedRoute.queryParams.subscribe({
+  //     next:(params)=>(this.order = params['order']),
+  //   });
+  //   this.destroyRef.onDestroy(()=> subscription.unsubscribe());
+  // }
 }
