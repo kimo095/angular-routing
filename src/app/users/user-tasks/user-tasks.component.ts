c8a6cb41cb9099eb049021d19f2c9665da39f5d2
kimo095 <eng095karim@gmail.com>
2026-07-20 10:47:22 +0200

    first commit

---
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -0,0 +1,32 @@
+import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
+import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterOutlet , RouterLink} from '@angular/router';
+
+
+@Component({
+  selector: 'app-user-tasks',
+  standalone: true,
+  imports:[RouterOutlet , RouterLink],
+  templateUrl: './user-tasks.component.html',
+  styleUrl: './user-tasks.component.css',
+})
+export class UserTasksComponent implements OnInit {
+  userId = input.required<string>();
+  private userService = inject(UsersService);
+  private destroyRef = inject(DestroyRef);
+
+  // userName = computed(()=>this.userService.users.find(u=>u.id === this.userId())?.name)
+
+  //Another way when dealing with older angular versions
+  username = '';
+  private activatedRoute = inject(ActivatedRoute)
+  ngOnInit(): void {
+      const subscription = this.activatedRoute.paramMap.subscribe({
+        next:(paramMap)=> {
+          this.username = this.userService.users.find((u)=>u.id === paramMap.get('userId'))?.name || ''
+        }
+      })
+      this.destroyRef.onDestroy(()=>subscription.unsubscribe())
+  }
+
+}
