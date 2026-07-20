def48ab98d14f77c18ec5363bb164f490a87fccb
kimo095 <80036845+kimo095@users.noreply.github.com>
2026-07-20 16:14:52 +0200

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,15 +4,39 @@ import { NoTaskComponent } from './tasks/no-task/no-task.component';
 import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
 
+// to protect some of your routes , you can use canActivate , canActivateChild? canMatch , ...
+// so those above are equalt to an array in which you can pass a method you pass a method inside it
+// to decide what will go next
+
+const dummyCanMatch: CanMatchFn = (route , segments ) => {
+  const router = inject(Router);
+  const shouldGetAccess = Math.random();
+  if (shouldGetAccess < 0.5 ){
+    return true;
+  }
+  return new RedirectCommand(router.parseUrl('/unauthorized'));
 
 export const routes:Routes = [
     {
         path:'',
-        component:NoTaskComponent   
+        component:NoTaskComponent 
     },
     {
         path:'users/:userId',//<your-domain>/tasks
         component:UserTasksComponent,
+      canMatch:[dummyCanMatch],
+      runGuardsAndResolvers: "always: // here i make sure the resolve work if the paramschange or anything
+            // here you can pass static data and recive as input in your component with help of input function
+      // since routing with input binding is activated 
+      data:{
+        message: 'hello'
+    }, 
+      //accepting dynamic data 
+      resove:{
+        userName: resolvedUserName
+      }
+      // you can do the same with title , to get dynamic ( resolved title)
+      title:resolveTitle // you do the same and this function in the component you use for
       // we add children here to add nested routes
         children:[
             {
@@ -28,18 +52,13 @@ export const routes:Routes = [
             },
             {
         path:'tasks/new',
-        component:NewTaskComponent
+        component:NewTaskComponent,
+    //angular let you add title in the page u are currently in by adding this property
+        title:' add a new task',
+              // to ask the user if they want to leave this page in this route
+              canDeactivate:[canLeaveEditPage]
             }
         ],
-      // here you can pass static data and recive as input in your component with help of input function
-      // since routing with input binding is activated 
-      data:{
-        message: 'hello'
-    }, 
-      //accepting dynamic data 
-      resove:{
-        userName: resolvedUserName
-      }
     },
   {
     path: '**',
